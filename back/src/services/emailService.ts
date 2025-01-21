bdb52d123076e3f921cc48202fc71b3606d87bca
victor haettinger <elie.lajoinie@supdevinci-edu.fr>
2025-01-21 14:29:17 +0100

    Securisation mail

--- back/src/services/emailService.ts
+++ back/src/services/emailService.ts
@@ -1,10 +1,12 @@
 import nodemailer from 'nodemailer';
+import dotenv from "dotenv";
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "elielajoinie24@gmail.com",
-    pass: "zwzl rmfx yytn qgqw",
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
